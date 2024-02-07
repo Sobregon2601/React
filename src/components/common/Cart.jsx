2d9228e9bbe50675e8ba5f1b563f14dcca7f825d
Mathias Obregon <obregonmathias@hotmail.com>
2024-02-07 18:45:38 -0300

    Primer preentrega

--- src/components/common/Cart.jsx
+++ src/components/common/Cart.jsx
@@ -4,7 +4,7 @@ import { TiShoppingCart } from "react-icons/ti";
 const Cart = () => {
   return (
     <div className="carrito">
-      <h3>carrito</h3>
+      <h2>carrito</h2>
       <span>10</span>
       <TiShoppingCart color="blue" />
     </div>
